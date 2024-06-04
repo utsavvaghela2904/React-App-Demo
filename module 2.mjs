4a74956e60195932dd1ae8e6c16d0bc77175e769
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-06-04 18:39:28 +0530

    ADD

---
+++ module 2.mjs
@@ -0,0 +1,9 @@
+const a = "UTsav";
+const b = "Yash";
+const c = "Aryan";
+const d = "Raj";
+
+export default a;
+export {b};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import './App.css';
-import img1 from './img/ai-art.webp';
-let name= "SIT";
 
 function App() {
   return (
    <>
-   <header>
-    <h1 className='SIT'>{name}</h1>
-    <nav>
-      <ul>
-        <li type="none">Home</li>
-        <li type="none">About</li>
-        <li type="none">Contact Us</li>
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SIT</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
       </ul>
-    </nav>
-    <hr />
-    <p className="lorem1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt laudantium velit placeat labore magnam dolorum debitis similique aliquid atque repellendus vel sapiente totam et impedit ex, eos pariatur deleniti.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam quos magni minima harum animi iure dolorem, et voluptatum nam, a illo nihil pariatur explicabo quia recusandae dolor iusto beatae.</p>
-
-    <img src="https://cdn.clickworker.com/wp-content/uploads/2022/09/BenefitsofAI.webp" alt="" />
-
-    <p className="lorem1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt laudantium velit placeat labore magnam dolorum debitis similique aliquid atque repellendus vel sapiente totam et impedit ex, eos pariatur deleniti.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam quos magni minima harum animi iure dolorem, et voluptatum nam, a illo nihil pariatur explicabo quia recusandae dolor iusto beatae.</p>
-
-    <img src={img1} alt="img"/>
-
-    <p className="lorem1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nesciunt laudantium velit placeat labore magnam dolorum debitis similique aliquid atque repellendus vel sapiente totam et impedit ex, eos pariatur deleniti.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam quos magni minima harum animi iure dolorem, et voluptatum nam, a illo nihil pariatur explicabo quia recusandae dolor iusto beatae.</p>
-   </header>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
    </>
   );
 }
