767815db6b969607a7e9e2076015638a1cbd7f1c
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-06-06 18:00:08 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar.js';
-import TextForm from './components/TextForm.js';
 
 function App() {
   return (
     <>
-      <Navbar title="SIT" aboutText="Contact Us"/>
-      <TextForm heading="Enter Your Text To Analyse Below"/>
+      
     </>
   );
 }
