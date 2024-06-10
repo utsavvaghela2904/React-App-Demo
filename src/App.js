42dd6860e16c6b58658a14331dd4709b1539e287
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-06-10 15:51:40 +0530

    add button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm.js';
 
 function App() {
+  const[mode,setMode]= useState('light')
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background='black'
+    } else {
+      setMode('light')
+      document.body.style.background='white'
+    }
+  }
   return (
     <>
-      <Navbar title="SIT" aboutText="Contact Us" />
-      <TextForm heading="Enter Your Text To Analyse Below" />
+      <Navbar title="SIT" aboutText="Contact Us" mode ={mode} toggleMode={toggleMode}/>
+      <TextForm heading="Enter Text To Analyse Below"/>
     </>
   );
 }
