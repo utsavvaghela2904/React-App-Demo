199f49ac7270dbc92195bc35ea700bb21e1da51d
utsavvaghela2904 <utsavvaghela2904@gmail.com>
2024-06-12 17:37:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -5,26 +5,83 @@ import TextForm from './components/TextForm.js';
 
 function App() {
   const [mode, setMode] = useState('light');
+  
 
-  const [btnText, newbtnText] = useState('Enble Dark Mode');
+  const [btnText, newbtnText] = useState(' Dark Mode');
 
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.background = 'black'
       document.body.style.color = 'white'
-      newbtnText("Enble Light Mode")
+      newbtnText("Light Mode")
     } else {
       setMode('light')
       document.body.style.color = 'black'
       document.body.style.background = 'white'
-      newbtnText("Enble Dark Mode")
+      newbtnText("Dark Mode")
+    }
+  }
+
+  const toggleMode1 = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = 'red'
+      document.body.style.color = 'white'
+      newbtnText("Red Mode")
+    } else {
+      setMode('light')
+      document.body.style.color = 'black'
+      document.body.style.background = 'white'
+      newbtnText("Dark Mode")
+    }
+  }
+
+  const toggleMode2 = () => {
+    if (mode === 'green') {
+      setMode('dark')
+      document.body.style.background = 'green'
+      document.body.style.color = 'white'
+      newbtnText("Green Mode")
+    } else {
+      setMode('light')
+      document.body.style.color = 'black'
+      document.body.style.background = 'white'
+      newbtnText("Dark Mode")
+    }
+  }
+
+  const toggleMode3 = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = 'yellow'
+      document.body.style.color = 'white'
+      newbtnText("Yellow Mode")
+    } else {
+      setMode('light')
+      document.body.style.color = 'black'
+      document.body.style.background = 'white'
+      newbtnText("Dark Mode")
+    }
+  }
+
+  const toggleMode4 = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = 'purple'
+      document.body.style.color = 'white'
+      newbtnText("Light Mode")
+    } else {
+      setMode('light')
+      document.body.style.color = 'black'
+      document.body.style.background = 'white'
+      newbtnText("Dark Mode")
     }
   }
   return (
     <>
-      <Navbar title="SIT" aboutText="Contact  Us" mode={mode} toggleMode={toggleMode} btnText={btnText} />
-      <TextForm heading="Enter Text To Analyse Below" mode={mode} toggleMode={toggleMode} />
+      <Navbar title="SIT" aboutText="Contact  Us" mode={mode} toggleMode={toggleMode} toggleMode1={toggleMode1} toggleMode2={toggleMode2} toggleMode3={toggleMode3} toggleMode4={toggleMode4} btnText={btnText} />
+      <TextForm heading="Enter Text To Analyse Below" mode={mode}  toggleMode={toggleMode} toggleMode1={toggleMode1} toggleMode2={toggleMode2} toggleMode3={toggleMode3} toggleMode4={toggleMode4} />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Navbar(props) {
     return (
         <div>
-            <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
+            <nav className={`navbar navbar-expand-lg bg-${props.mode} bg-${props.mode1} navbar-${props.mode}`} >
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,6 +15,32 @@ export default function Navbar(props) {
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><div class={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode} />{props.btnText}
+                                    </div></li>
+                                    <li><div class={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode1} />{props.btnText}
+                                    </div></li>
+                                    <li><div class={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode2} />{props.btnText}
+                                    </div></li>
+                                    <li>
+                                        <div class={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode3} />{props.btnText}
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode4} />{props.btnText}
+                                        </div>
+                                    </li>
+                                </ul>
+                            </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Link</a>
                             </li>
@@ -22,11 +48,6 @@ export default function Navbar(props) {
                                 <a className="nav-link disabled" aria-disabled="true">{props.aboutText}</a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <div class={`form-check form-switch text-${props.mode === 'light' ? 'dark': 'light'}`}>
-                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.toggleMode}/>{props.btnText}
-                            </div>
-                        </form>
                     </div>
                 </div>
             </nav>
